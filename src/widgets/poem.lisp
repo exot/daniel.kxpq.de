b46ca9cafe0395908ab831cb80aeaa91427d934c
Daniel Borchmann <daniel.borchmann@mailbox.tu-dresden.de>
2011-09-15 18:06:28 +0200

    correctly update updated poems
    Signed-off-by: Daniel Borchmann <daniel.borchmann@mailbox.tu-dresden.de>

--- src/widgets/poem.lisp
+++ src/widgets/poem.lisp
@@ -8,11 +8,10 @@
          :initarg  :poem
          :type poem)))
 
-(defmethod render-widget ((poem-widget standard-poem-widget) &key &allow-other-keys)
+(defmethod render-widget-body ((poem-widget standard-poem-widget) &key &allow-other-keys)
   (let ((poem (poem poem-widget)))
     (with-html
-      (:div :class "widget standard-poem"
-            :style "padding-top:3ex;padding-bottom:3ex;"
+      (:div :style "padding-top:3ex;padding-bottom:3ex;"
         (when (poem-title poem)
           (htm (:div :class "poem-title"
                  (str (poem-title poem)))))
@@ -33,20 +32,19 @@
          :initarg  :mode
          :type     (or :show :folded))))
 
-(defmethod render-widget ((widget foldable-poem-widget) &key &allow-other-keys)
+(defmethod render-widget-body ((widget foldable-poem-widget) &key &allow-other-keys)
   (print (mode widget))
   (ecase (mode widget)
     (:show
      (with-html
-       (:div :class "widget unfolded-poem"
-         (:div :style "text-align:right"
-           (render-link (f_% (setf (mode widget) :folded))
-                        "Dismiss"))
-         (render-widget (poem-widget widget)))))
+       (:div :style "text-align:right"
+         (render-link (f_% (setf (mode widget) :folded))
+                      "Dismiss"))
+       (render-widget (poem-widget widget))))
     (:folded
      (let ((poem (poem (poem-widget widget))))
        (with-html
-         (:div :class "widget folded-poem"
+         (:div :class "folded-poem"
            (render-link (f_% (setf (mode widget) :show))
                         (or (poem-title poem)
                             (concatenate
@@ -65,8 +63,6 @@
                                :poem-widget (make-instance poem-widget-class
                                                            :poem poem)
                                :mode mode)))
-    (setf (widget-propagate-dirty widget)
-          (list main-widget (widget-propagate-dirty widget)))
     widget))
 
 ;;;
@@ -80,8 +76,8 @@
   (declare (ignore args))
   (with-html
     (:div :class "poem-selector"
-          (:h2 "Selected Poems")
-          (:p "Here you may find a very personal collection of poems I like."))))
+      (:h2 "Selected Poems")
+      (:p "Here you may find a very personal collection of poems I like."))))
 
 (defmethod render-widget-children ((obj poem-selector) &rest args)
   (with-html (:hr))
