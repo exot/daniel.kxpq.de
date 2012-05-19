a2e2f3a37518358b0bd0da6274f832b4ba60aef4
Daniel Borchmann <daniel.borchmann@mailbox.tu-dresden.de>
2012-05-19 22:18:09 +0200

    No debugging anymore
    Signed-off-by: Daniel Borchmann <daniel.borchmann@mailbox.tu-dresden.de>

--- website.lisp
+++ website.lisp
@@ -30,7 +30,7 @@
     :ignore-default-dependencies nil
     :public-files-path #P"/home/borch/Documents/lang/lisp/cl/source/website/pub/"
     :autostart nil
-    :debug t
+    :debug nil
     :html-indent-p t
     :dependencies  '((:script "http://cdn.mathjax.org/mathjax/latest/MathJax.js?config=TeX-AMS-MML_HTMLorMML")
                      (:script "mathjax"))
