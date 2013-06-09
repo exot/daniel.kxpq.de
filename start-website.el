4b4eb50c24bb3823246180a34de97c24d5c78c07
Daniel Borchmann <daniel.borchmann@mailbox.tu-dresden.de>
2013-06-09 04:46:29 +0200

    Update startup scripts
    Signed-off-by: Daniel Borchmann <daniel.borchmann@mailbox.tu-dresden.de>

--- start-website.el
+++ start-website.el
@@ -1,5 +1,7 @@
+(require 'slime)
 (slime-start :program "ccl"
              :directory (expand-file-name "~/Documents/lang/lisp/cl/source/website/")
              :program-args '("-e" "(progn
+                                     (ql:quickload :weblocks-memory)
                                      (ql:quickload :website)
                                      (funcall (read-from-string \"website:start-website\")))"))
